da8df3c8da45c091a659b0f8f418e5d21c83e597
mcsans <mihsansaepulloh9@gmail.com>
2023-09-21 11:07:57 +0700

    [i][fix] event duplicate tbldisplayhistory

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -28,40 +28,60 @@ class Task extends CI_Controller {
 
 	public function transData() {
 		// TABLE DISPLAY PROSES
-		$displayProses = $this->wanfeng->query("SELECT * FROM tbldisplayproses  WHERE start_time BETWEEN  CURDATE() - INTERVAL 1 DAY AND NOW()")->result_array();
+		$displayProses = $this->wanfeng->query("SELECT * FROM tbldisplayproses WHERE start_time BETWEEN CURDATE() - INTERVAL 1 DAY AND NOW() LIMIT 2")->result_array();
 
-		$sql = "INSERT INTO tbldisplayproses (id, id_wo, id_wo_ori, proses, id_grey, nama_unit, nama_proses, user, lastmodified, start_time, end_time, j_kain) VALUES ";
-		$update_sql = " ON DUPLICATE KEY UPDATE id = VALUES(id), id_wo = VALUES(id_wo), id_wo_ori = VALUES(id_wo_ori), proses = VALUES(proses), id_grey = VALUES(id_grey), nama_unit = VALUES(nama_unit), nama_proses = VALUES(nama_proses), user = VALUES(user), lastmodified = VALUES(lastmodified), start_time = VALUES(start_time), end_time = VALUES(end_time), j_kain = VALUES(j_kain)";
+		$dataToInsert = [];
 
-		$value_strings = array();
-		$values = array();
-		foreach ($displayProses as $row) {
-				$value_strings[] = '(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-				$values = array_merge($values, array_values($row));
+		foreach ($displayProses as $item) {
+				$id = $item['id'];
+				$id_wo = $item['id_wo'];
+				$id_wo_ori = $item['id_wo_ori'];
+				$proses = $item['proses'];
+				$id_grey = $item['id_grey'];
+				$nama_unit = $item['nama_unit'];
+				$nama_proses = $item['nama_proses'];
+				$user = $item['user'];
+				$lastmodified = $item['lastmodified'];
+				$start_time = $item['start_time'];
+				$end_time = $item['end_time'];
+				$j_kain = $item['j_kain'];
+
+				$dataToInsert[] = "('$id', '$id_wo', '$id_wo_ori', '$proses', '$id_grey', '$nama_unit', '$nama_proses', '$user', '$lastmodified', '$start_time', '$end_time', '$j_kain')";
 		}
 
-		$sql .= implode(', ', $value_strings);
-		$sql .= $update_sql;
+		$valuesClause = implode(', ', $dataToInsert);
 
-		$this->server->query($sql, $values);
+		$sql = "INSERT IGNORE INTO tbldisplayproses (id, id_wo, id_wo_ori, proses, id_grey, nama_unit, nama_proses, user, lastmodified, start_time, end_time, j_kain) VALUES $valuesClause";
 
-		// TABLE DISPLAY PROSES HISTORY
-		$displayProsesHistory = $this->wanfeng->query('SELECT * FROM tbldisplayproseshistory   WHERE start_time BETWEEN  CURDATE() - INTERVAL 1 DAY AND NOW()')->result_array();
+		$this->server->query($sql);
 
-		$sql = "INSERT INTO tbldisplayproseshistory (id_produksi, id_wo, id_wo_ori, id_dpr, id_kode, kode, proses, waktu, operator, lastmodified, start_time, end_time) VALUES ";
-		$update_sql = " ON DUPLICATE KEY UPDATE id_produksi = VALUES(id_produksi), id_wo = VALUES(id_wo), id_wo_ori = VALUES(id_wo_ori), id_dpr = VALUES(id_dpr), id_kode = VALUES(id_kode), kode = VALUES(kode), proses = VALUES(proses), waktu = VALUES(waktu), operator = VALUES(operator), lastmodified = VALUES(lastmodified), start_time = VALUES(start_time), end_time = VALUES(end_time)";
+		// TABLE DISPLAY PROSES HISTORY===============
+		$displayProsesHistory = $this->wanfeng->query('SELECT * FROM tbldisplayproseshistory WHERE start_time BETWEEN CURDATE() - INTERVAL 1 DAY AND NOW()')->result_array();
 
-		$value_strings = array();
-		$values = array();
-		foreach ($displayProsesHistory as $row) {
-				$value_strings[] = '(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-				$values = array_merge($values, array_values($row));
+		$dataToInsert = [];
+
+		foreach ($displayProsesHistory as $item) {
+				$id_produksi = $item['id_produksi'];
+				$id_wo = $item['id_wo'];
+				$id_wo_ori = $item['id_wo_ori'];
+				$id_dpr = $item['id_dpr'];
+				$id_kode = $item['id_kode'];
+				$kode = $item['kode'];
+				$proses = $item['proses'];
+				$waktu = $item['waktu'];
+				$operator = $item['operator'];
+				$lastmodified = $item['lastmodified'];
+				$start_time = $item['start_time'];
+				$end_time = $item['end_time'];
+
+				$dataToInsert[] = "('$id_produksi', '$id_wo', '$id_wo_ori', '$id_dpr', '$id_kode', '$kode', '$proses', '$waktu', '$operator', '$lastmodified', '$start_time', '$end_time')";
 		}
 
-		$sql .= implode(', ', $value_strings);
-		$sql .= $update_sql;
+		$valuesClause = implode(', ', $dataToInsert);
 
-		$this->server->query($sql, $values);
+		$sql = "INSERT IGNORE INTO tbldisplayproseshistory (id_produksi, id_wo, id_wo_ori, id_dpr, id_kode, kode, proses, waktu, operator, lastmodified, start_time, end_time) VALUES $valuesClause";
+
+		$this->server->query($sql);
 
 		$this->updateLastruntimeCount('transData');
 	}
